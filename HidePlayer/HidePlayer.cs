4919c6c92e1dd69f396bf38f74dd1eaf2135e9d3
raytzou <raytzou@outlook.com>
2025-11-09 01:46:21 +0800

    chore: add plugin info

--- HidePlayer/HidePlayer.cs
+++ HidePlayer/HidePlayer.cs
@@ -1,7 +1,14 @@
-﻿namespace HidePlayer
-{
-    public class HidePlayer
-    {
+﻿using CounterStrikeSharp.API.Core;
 
+namespace HidePlayer
+{
+    public class HidePlayer : BasePlugin
+    {
+        #region plugin info
+        public override string ModuleName => "Hide Player";
+        public override string ModuleVersion => "0.87";
+        public override string ModuleAuthor => "cynicat";
+        public override string ModuleDescription => "A plugin that registers command to hide player from the same team";
+        #endregion plugin info
     }
 }
